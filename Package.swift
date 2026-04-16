4666ecfa1baafd03ea5196de700d310e55d37777
MobileID <123370216+vbmobile@users.noreply.github.com>
2026-04-16 11:23:44 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,15 +13,15 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/vbmobile/AMADocModeliOS", exact: "1.0.0-rc20"),
+        .package(url: "https://github.com/vbmobile/AMADocModeliOS", exact: "1.0.0-rc23"),
         .package(url: "https://github.com/regulaforensics/DocumentReader-Swift-Package", from: "8.3.0"),
         .package(url: "https://github.com/regulaforensics/DocumentReaderMRZRFID-Swift-Package", from: "8.3.0"),
     ],
     targets: [
         .binaryTarget(
             name: "mdi-mob-sdk-doc-mrz-regula-ios",
-            url: "https://vbmobileidstorage.blob.core.windows.net/ios/AMADocScanRegulaiOS/mdi-mob-sdk-doc-mrz-regula-ios-1.0.0-rc22.zip",
-            checksum: "a14434054f7031744060dc12d81a0615221b15f7595c5104fd48af674820a96d"
+            url: "https://vbmobileidstorage.blob.core.windows.net/ios/AMADocScanRegulaiOS/mdi-mob-sdk-doc-mrz-regula-ios-1.0.0-rc17.zip",
+            checksum: "e3a186bfda66a0b40c2bc9fbf90a60ad2783e2342205a3e30754e712a0fd11ec"
         ),
         .target(
             name: "AMADocScanRegulaiOS",
